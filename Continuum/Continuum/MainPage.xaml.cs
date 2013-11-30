f4dcbde796042a932c22e72facdcd6575acaefa0
Chosko <ruben.caliandro@gmail.com>
2013-11-30 02:49:34 +0100

    new UI

--- Continuum/Continuum/MainPage.xaml.cs
+++ Continuum/Continuum/MainPage.xaml.cs
@@ -16,7 +16,6 @@ namespace Continuum
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        bool isButtonAvailable = false;
 
         // Constructor
         public MainPage()
@@ -31,22 +30,7 @@ namespace Continuum
 
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
-            Apertura.Begin();
-            Apertura.Completed += new EventHandler(Apertura_Completed);
-            if (Float1.GetCurrentTime().Milliseconds > 0)
-                Float1.Stop();
-            if (Float2.GetCurrentTime().Milliseconds > 0)
-                Float2.Stop();
             base.OnNavigatedTo(e);
-        }
-
-        void Apertura_Completed(object sender, EventArgs e)
-        {
-            Float1.Begin();
-            Float2.Begin();
-            Float1.RepeatBehavior = RepeatBehavior.Forever;
-            Float2.RepeatBehavior = RepeatBehavior.Forever;
-            isButtonAvailable = true;
         }
 
         protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
@@ -54,29 +38,9 @@ namespace Continuum
             base.OnNavigatedFrom(e);
         }
 
-        void Chiusura_Completed(object sender, EventArgs e)
-        {
-            NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
-        }
-
         private void gamePage_Click(object sender, RoutedEventArgs e)
         {
-            if (isButtonAvailable)
-            {
-                isButtonAvailable = false;
-                Float1.Pause();
-                Float2.Pause();
-                Chiusura.Begin();
-                Chiusura.Completed += new EventHandler(Chiusura_Completed);
-            }
-            else
-            {
-                Apertura.Stop();
-                Chiusura.Stop();
-                Float1.Stop();
-                Float2.Stop();
-                NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
-            }
+            NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
         }
 
         private void tutorial_Click(object sender, RoutedEventArgs e)
@@ -89,7 +53,7 @@ namespace Continuum
             NavigationService.Navigate(new Uri("/ScorePage.xaml", UriKind.Relative));
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void credits_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/Credits.xaml", UriKind.Relative));
         }
